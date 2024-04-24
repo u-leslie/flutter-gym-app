ba44e345837f153b2835b6c7e319c7d0d1cce906
Uhiriwe Anne Leslie <heavenmadeleslie@gmail.com>
2024-04-24 15:15:17 +0200

    workout gym app

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:workout_app/screens/welcom_view.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WelcomView(),
+    );
+  }
+}
